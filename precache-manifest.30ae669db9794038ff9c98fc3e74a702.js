4929db9df6e8a4b46bc7d2c23b796f1a9d613bdc
Cale Shapera <caleshapera@gmail.com>
2019-09-05 11:24:23 -0700

    Updates

---
+++ precache-manifest.30ae669db9794038ff9c98fc3e74a702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef20a7c0a6189032a70deb004d99f36",
+    "url": "/dbfront/index.html"
+  },
+  {
+    "revision": "a3c52c26de2e5968b0f0",
+    "url": "/dbfront/static/css/main.995f9c06.chunk.css"
+  },
+  {
+    "revision": "7e5155354270e826432d",
+    "url": "/dbfront/static/js/2.382ccb15.chunk.js"
+  },
+  {
+    "revision": "a3c52c26de2e5968b0f0",
+    "url": "/dbfront/static/js/main.788e480b.chunk.js"
+  },
+  {
+    "revision": "a2c1978de3b20e73e145",
+    "url": "/dbfront/static/js/runtime~main.707a6ba0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dbfront/static/media/logo.5d5d9eef.svg"
+  }
+]);
